def39fb88e630fefa431c4176889ee2d75c5d81a
Kiandra Ginevra <kiandrajayne@gmail.com>
2020-08-29 16:40:47 +1000

    Updated detect pangram solution

--- 6kyu/detect-pangram.js
+++ 6kyu/detect-pangram.js
@@ -2,7 +2,6 @@ function isPangram(string){
     let abc = [ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' ]
   
     let str = string.toLowerCase().split(' ').join('').split('')
-    let result = true
     
     for (let i = 0; i < abc.length; i++) {
         if (!str.includes(abc[i]))
